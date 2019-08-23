9a010ab2dc68b8d75a8e88ea7d19b534977aa64b
Ben <bnsd55@gmail.com>
2019-08-23 22:26:36 +0300

    Updates

---
+++ precache-manifest.2ebcbbebfda12ec6b62f2c3bc3c115f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a618de0f14ddcc8023c6fea186ab5d",
+    "url": "/react-cryptocurrency/index.html"
+  },
+  {
+    "revision": "ef51b9b2e8fe8cae1b90",
+    "url": "/react-cryptocurrency/static/css/main.9ff73ec5.chunk.css"
+  },
+  {
+    "revision": "a4bd27553d636454eb9b",
+    "url": "/react-cryptocurrency/static/js/2.20e08291.chunk.js"
+  },
+  {
+    "revision": "ef51b9b2e8fe8cae1b90",
+    "url": "/react-cryptocurrency/static/js/main.13eeb895.chunk.js"
+  },
+  {
+    "revision": "6b9d88ab994f1fcc5f41",
+    "url": "/react-cryptocurrency/static/js/runtime~main.f5b7d2bb.js"
+  }
+]);
